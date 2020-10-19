1f926a7aab49124228e35989b71a41548fa92ea1
Natalia <n8galan@gmail.com>
2020-10-19 15:05:32 -0500

    generated user and video resource, seeded data and tested relationships sucessfully in the console

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Video.destroy_all
+User.destroy_all
+
+u1 = User.create(username: 'ursula')
+
+v1 = Video.create(url: 'https://youtu.be/VEUe1J0c2k4', lyrics: 'cant you bear just one more night?', user_id: User.ids.sample)
+
+v2 = Video.create(url: 'https://youtu.be/AuupPbyDs7s', lyrics: 'bam bam', user_id: User.ids.sample)
+
+v3 = Video.create(url: 'https://youtu.be/3ALuST7XCGA', lyrics: 'widowspeak plus', user_id: User.ids.sample)
+
