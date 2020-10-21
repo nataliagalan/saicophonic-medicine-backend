833253a48742bcc6adbc4d0725b573c7e24aaff7
Natalia <n8galan@gmail.com>
2020-10-21 10:57:41 -0500

    full crud implemented, making this commit before expanding schema to add timestamp functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :videos
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :videos
+      resources :users
+      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    end
+  end
 end
