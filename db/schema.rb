3bf05de04ef1114e49f5ada73402dfdedd81a7dd
Natalia Galán <hi@nataliagalan.me>
2020-10-21 11:29:20 -0500

    generated resource for song and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_162220) do
+ActiveRecord::Schema.define(version: 2020_10_21_162536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "songs", force: :cascade do |t|
+    t.integer "timestamp"
+    t.text "lyrics"
+    t.string "title"
+    t.bigint "video_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["video_id"], name: "index_songs_on_video_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -29,5 +39,6 @@ ActiveRecord::Schema.define(version: 2020_10_21_162220) do
     t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
+  add_foreign_key "songs", "videos"
   add_foreign_key "videos", "users"
 end
