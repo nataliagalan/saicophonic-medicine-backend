72907dc56204e76f5dfa61d3a3c5c3c4942440c4
Natalia Galán <hi@nataliagalan.me>
2020-10-21 11:38:58 -0500

    created seed data and tested relationships, all working

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Song.destroy_all
 Video.destroy_all
 User.destroy_all
 
+
 u1 = User.create(username: 'ursula')
 
-v1 = Video.create(url: 'https://youtu.be/VEUe1J0c2k4', lyrics: 'cant you bear just one more night?', user_id: User.ids.sample)
+v1 = Video.create(url: 'https://youtu.be/VEUe1J0c2k4', user_id: User.ids.sample)
 
-v2 = Video.create(url: 'https://youtu.be/AuupPbyDs7s', lyrics: 'bam bam', user_id: User.ids.sample)
+v2 = Video.create(url: 'https://youtu.be/AuupPbyDs7s', user_id: User.ids.sample)
 
-v3 = Video.create(url: 'https://youtu.be/3ALuST7XCGA', lyrics: 'widowspeak plus', user_id: User.ids.sample)
+v3 = Video.create(url: 'https://youtu.be/3ALuST7XCGA', user_id: User.ids.sample)
 
+s1 = Song.create(timestamp: 341, lyrics: 'test lyrics', title: 'test title', video_id: Video.ids.sample)
+s2 = Song.create(timestamp: 538, lyrics: 'test lyrics2', title: 'test title2', video_id: Video.ids.sample)
