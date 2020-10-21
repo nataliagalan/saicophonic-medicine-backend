527609ef11a005c9b61c4d4cef1dbc01156b3a7f
Natalia <n8galan@gmail.com>
2020-10-21 12:03:52 -0500

    implemented video serializer for index action

---
+++ app/services/video_serializer.rb
@@ -0,0 +1,19 @@
+class VideoSerializer
+ 
+  def initialize(video_object)
+    @video = video_object
+  end
+ 
+  def to_serialized_json
+    options = {
+      include: {
+        songs: {
+          only: [:lyrics, :timestamp, :title, :video_id]
+        }
+      },
+      except: [:updated_at, :created_at],
+    }
+    @video.to_json(options)
+  end
+
+end #end of vs
