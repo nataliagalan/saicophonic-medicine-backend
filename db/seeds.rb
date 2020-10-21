c905e1f8d11f82fba7a069f5d294a7893ee45333
Natalia Galán <hi@nataliagalan.me>
2020-10-21 13:00:12 -0500

    destroy action working

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,6 @@ v2 = Video.create(url: 'https://youtu.be/AuupPbyDs7s', user_id: User.ids.sample)
 
 v3 = Video.create(url: 'https://youtu.be/3ALuST7XCGA', user_id: User.ids.sample)
 
-# s1 = Song.create(timestamp: 341, lyrics: 'test lyrics', title: 'test title', video_id: Video.ids.sample)
-# s2 = Song.create(timestamp: 538, lyrics: 'test lyrics2', title: 'test title2', video_id: Video.ids.sample)
-# s3 = Song.create(timestamp: 638, lyrics: 'test lyrics3', title: 'test title3', video_id: Video.ids.sample)
-# s4 = Song.create(timestamp: 738, lyrics: 'test lyrics4', title: 'test title4', video_id: Video.ids.sample)
-
-
 Video.all.each do |video|
   rand(1..3).times do
     Song.create(timestamp: 638, lyrics: 'test lyrics', title: 'test title', video_id: video.id)
