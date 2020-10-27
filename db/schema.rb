66660e5598cac59445c74d0464831cf45201a98d
Natalia <n8galan@gmail.com>
2020-10-27 16:59:08 -0500

    implemented authentication, reseeded, added auth route and controller, added pw digest column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_162536) do
+ActiveRecord::Schema.define(version: 2020_10_27_201501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_10_21_162536) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   create_table "videos", force: :cascade do |t|
