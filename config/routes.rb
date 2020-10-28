23164c0f2024ad19a5ee53357acb361907689b05
Natalia Galán <hi@nataliagalan.me>
2020-10-27 22:03:28 -0500

    successfully implemented authorization with jwt for Login and New pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       resources :users
       resources :songs
       post '/auth', to: 'auth#create'
-      # get 'current/user', to: 'auth#show'
+      get 'current_user', to: 'auth#show'
       # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     end
   end
