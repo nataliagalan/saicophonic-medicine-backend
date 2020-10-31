13d6ddbc8125e0ada110bdf4e35943c627e0f3fc
Natalia <n8galan@gmail.com>
2020-10-30 21:29:02 -0500

    elasticsearch is rendering custom fields, ignoring unnecessary fields like id or url, also allows for some misspellings!!!!

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,7 @@
 class Song < ApplicationRecord
   belongs_to :video
 
+  #this will index the Song model whenever a record is created or updated or destroyed
   searchkick word_middle: [:title, :lyrics]
 
   after_commit :reindex_video
