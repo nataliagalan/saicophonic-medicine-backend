fdc14cbe16bf7c13b2bfa10e108aebd921a65ccd
Natalia Galán <hi@nataliagalan.me>
2020-10-30 21:29:02 -0500

    elasticsearch is rendering custom fields, ignoring unnecessary fields like id or url, also allows for some misspellings!!!!

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -5,10 +5,50 @@ class Api::V1::VideosController < ApplicationController
     render json: VideoSerializer.new(videos).to_serialized_json
   end
 
+  # class << self
+  #   def query(params)
+  #     listings = self.active_record_search(params) # filter by other parameters first
+  
+  #     # return right there if search is blank
+  #     return listings.page(params[:page]) if params[:search].blank?
+  
+  #     # otherwise pass already filtered set to elastic search for further filtering
+  #     listing_ids = listings.pluck(:id)
+  #     self.elastic_search(params, listing_ids)
+  #   end
+  
+  #   def active_record_search(params)
+  #     # ...some code
+  #   end
+  
+  #   def elastic_search(params, listing_ids)
+  #     elastic_query = {
+
+  #       fields: [:song_title, :song_lyrics],
+  #     }
+  
+  #     self.search(params[:search], elastic_query)
+  #   end
+  # end
+
+
   #GET /search/:query
   def search
-    videos = Video.search params[:query], misspellings: {edit_distance: 2}
+    elastic_query = {
+      # fields: [:title, :description, :tagged, :artist_name],
+      fields: [:song_title, :song_lyrics],
+      misspellings: {edit_distance: 2}
+      # include a where clause so I'm only searching records returned by the other filters
+      # where: { id: listings_ids },
+      # order: { _score: :desc },
+      # page: params[:page],
+      # per_page: 15
+    }
+
+
+    videos = Video.search params[:query], elastic_query
     
+    # videos = Video.search params[:query], misspellings: {edit_distance: 2}
     puts "TOTAL COUNT #{videos.total_count }"
     if response
     # render :json => videos.to_json
@@ -17,6 +57,8 @@ class Api::V1::VideosController < ApplicationController
     render json: { error: 'did not find anything' }
     end
   end
+
+
 
   def show
     video = Video.find(params[:id])
--- app/models/video.rb
+++ app/models/video.rb
@@ -2,18 +2,33 @@ class Video < ApplicationRecord
   belongs_to :user
   has_many :songs
 
+  #validations go here too!
+
+  #this will index the Song model whenever a record is created or updated or destroyed
   searchkick word_middle: [:songs]
-
+  
   scope :search_import, -> { includes(:songs) }
+  
+  
+  #remember to always reindex after making changes here
 
-
+  #keys inside search_data describe how the data is indexed
   def search_data
-    attributes.merge(
-      songs_data: songs
-      # song_title: songs(&:title)
-      #You use the .map method for the has_many associations.
-      # subject_name: subjects.map(&:name)
-    )
+    { 
+      # song_title: songs.map(&:title),
+      song_title: "#{songs.map(&:title).join(' ')}",
+      song_lyrics: "#{songs.map(&:lyrics).join(' ')}"
+      # tagged: "#{tags.map(&:name).join(' ')}" 
+    }
+
+    #I think attributes.merge makes it so attributes are not nested under "_source"
+    # attributes.merge(
+    #   #You use the .map method for the has_many associations.
+    #   song_title: songs.map(&:title),
+    #   song_lyrics: songs.map(&:lyrics)
+    # )
   end
 
 end
+
+
