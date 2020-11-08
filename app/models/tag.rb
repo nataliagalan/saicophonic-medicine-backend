dc3b6aaf949379dec723a702446b3b2ee4ba37d7
Natalia Galán <hi@nataliagalan.me>
2020-11-07 22:18:53 -0600

    changed elasticsearch partial match to text middle on all models

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,7 +3,7 @@ class Tag < ApplicationRecord
   has_many :videos, through: :video_tags
 
     #this will index the Tag model whenever a record is created or updated or destroyed
-    searchkick word_middle: [:name]
+    searchkick text_middle: [:name]
 
     after_commit :reindex_video
   
