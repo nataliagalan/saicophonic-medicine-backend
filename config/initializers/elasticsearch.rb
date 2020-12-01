294db63719a4a7591948edf4d0e76c915eced8e9
Natalia <n8galan@gmail.com>
2020-12-01 16:13:20 -0600

    new elasticsearch config setup

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,8 +1,12 @@
-if Rails.env == 'production'
-  # url = ENV["BONSAI_URL"]
-  # transport_options = { request: { timeout: 250 } }
-  # options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
-  # Searchkick.client = Elasticsearch::Client.new(options)
-  Searchkick.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
+# if Rails.env == 'production'
+#   # url = ENV["BONSAI_URL"]
+#   # transport_options = { request: { timeout: 250 } }
+#   # options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
+#   # Searchkick.client = Elasticsearch::Client.new(options)
+#   Searchkick.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
   
+# end
+
+if ENV['BONSAI_URL']
+  Searchkick.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
 end
