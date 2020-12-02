2bc62390a885542945fc3f02e38198705f242e50
Natalia <n8galan@gmail.com>
2020-12-01 18:39:03 -0600

    debugging current_user path

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::AuthController < ApplicationController
 
   def create
+    
     user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       payload = { user_id: user.id }
@@ -13,7 +14,9 @@ class Api::V1::AuthController < ApplicationController
   end #end of create action
 
   def show
-    token = request.headers[:Authorization].split(' ').last
+    byebug
+    # token = request.headers[:Authorization].split(' ').last
+    token = request.headers[:Authorization]
     decoded_token = JWT.decode(token, ENV['jwt_secret'], true, { algorithm: 'HS256'})
     user_id = decoded_token[0]['user_id']
     user = User.find(user_id)
