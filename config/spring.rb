7e2f5a52b0b04ff82670be970aaf91d1af13b02f
Natalia <n8galan@gmail.com>
2020-12-14 19:34:28 -0600

    adds config/application.yml to spring.rb file

--- config/spring.rb
+++ config/spring.rb
@@ -2,10 +2,6 @@ Spring.watch(
   ".ruby-version",
   ".rbenv-vars",
   "tmp/restart.txt",
-  "tmp/caching-dev.txt"
+  "tmp/caching-dev.txt",
+  "config/application.yml"
 )
-
-%w(
-  ...
-  config/application.yml
-).each { |path| Spring.watch(path) }
