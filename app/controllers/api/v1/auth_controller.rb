b23625e9e6e52c29758b27d05ed7238582c256d8
Natalia Galán <hi@nataliagalan.me>
2020-12-14 22:24:16 -0600

    troubleshooting login issue

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -15,17 +15,25 @@ class Api::V1::AuthController < ApplicationController
 
   def show
     # byebug
-    token = request.headers['Authorization'].split(' ')[1]
-    # token = request.headers[:Authorization]
-    # decoded_token = JWT.decode(token, ENV['jwt_secret'], true, { algorithm: 'HS256'})
-    decoded_token = JWT.decode(token, 'S3k3R3T', false, { algorithm: 'HS256' })
-    user_id = decoded_token[0]['user_id']
-    user = User.find(user_id)
-    if user
-      render json: { id: user.id, username: user.username, token: token }
-    else
-      render json: { error: 'invalid token' }
+    if request.headers['Authorization']
+      token = request.headers['Authorization'].split(' ')[1]
     end
+    
+    if token
+      
+      # token = request.headers[:Authorization]
+      # decoded_token = JWT.decode(token, ENV['jwt_secret'], true, { algorithm: 'HS256'})
+      decoded_token = JWT.decode(token, 'S3k3R3T', false, { algorithm: 'HS256' })
+      user_id = decoded_token[0]['user_id']
+      user = User.find(user_id)
+      if user
+        render json: { id: user.id, username: user.username, token: token }
+      else
+        render json: { error: 'invalid token' }
+      end
+
+    end
+ 
   end #end of show action
 
 end #end of ac
