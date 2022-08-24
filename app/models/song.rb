d9005a7900229e0f28a61524e3cdd46a08bc94b5
Natalia Galán <hi@nataliagalan.me>
2022-08-23 19:54:15 -0500

    chore: test change to enable redeploy

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ApplicationRecord
   belongs_to :video
   validates :lyrics, :title, :timestamp, presence: true
 
-  #this will index the Song model whenever a record is created or updated or destroyed
+  #this will index the Song model whenever a record is created or updated or destroyeddd
   searchkick text_middle: [:title, :lyrics]
 
   after_commit :reindex_video
