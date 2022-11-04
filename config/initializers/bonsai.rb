83b567b1d5a96bca475dd58205eb6a527766ad74
Natalia Galán <hi@nataliagalan.me>
2022-11-04 16:00:12 -0500

    chore: test to redeploy

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -4,7 +4,6 @@
 #   # options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
 #   # Searchkick.client = Elasticsearch::Client.new(options)
 #   Searchkick.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
-  
 # end
 
 if ENV['BONSAI_URL']
