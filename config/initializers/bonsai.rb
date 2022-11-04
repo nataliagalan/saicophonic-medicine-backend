148fbe2956a542e08270a34e32e6e9ae454bc3d5
Natalia Galán <hi@nataliagalan.me>
2022-11-04 16:14:13 -0500

    fix: require elasticsearch

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -5,6 +5,7 @@
 #   # Searchkick.client = Elasticsearch::Client.new(options)
 #   Searchkick.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
 # end
+require 'elasticsearch'
 
 if ENV['BONSAI_URL']
   Searchkick.client = Elasticsearch::Client.new({url: ENV['BONSAI_URL'], logs: true})
