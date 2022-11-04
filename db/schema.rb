8596f2229bfe06cad607e6efcdca9dc969d2dbdb
Natalia Galán <hi@nataliagalan.me>
2022-11-04 13:02:53 -0500

    feat: migration to add bandcamp column to videos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_031252) do
+ActiveRecord::Schema.define(version: 2022_11_01_165150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_11_08_031252) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "band"
+    t.string "bandcamp"
     t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
